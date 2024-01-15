ef8dd18c72aa659975a8c41d64204b1d68eaa351
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2024-01-15 22:24:38 +0100

    websocket connection successfull with production

--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as Stomp from 'stompjs';
 import SockJS from 'sockjs-client';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,45 +8,34 @@ import { Observable } from 'rxjs';
 export class WebsocketService {
   private stompClient!: Stomp.Client;
 
-  constructor() {
-    this.initializeWebSocketConnection();
-  }
+  constructor() {}
 
-  initializeWebSocketConnection(): void {
-    const socket = new SockJS('https://club-lingua-backend.onrender.com/ws');
+  connect(nickname: string, fullname: string): void {
+    const socket = new SockJS('https://club-lingua-backend.onrender.com/ws'); // Update the WebSocket URL
     this.stompClient = Stomp.over(socket);
-  }
 
-  public connect(nickname: string, fullname: string): Observable<any> {
-    return new Observable((observer) => {
-      this.stompClient.connect(
-        {},
-        (frame) => {
-          observer.next(frame);
-          this.stompClient.subscribe(
-            `/user/${nickname}/queue/messages`,
-            (message) => {
-              observer.next(message);
-            }
-          );
-          this.stompClient.subscribe(`/user/public`, (message) => {
-            observer.next(message);
-          });
-          this.stompClient.send(
-            '/app/user.addUser',
-            {},
-            JSON.stringify({
-              nickName: nickname,
-              fullName: fullname,
-              status: 'ONLINE',
-            })
-          );
-        },
-        (error) => {
-          observer.error(error);
-        }
-      );
-    });
+    this.stompClient.connect(
+      {},
+      (frame) => {
+        console.log('Connected:', frame);
+        // Additional logic for handling successful connection
+
+        // You can send data or do other operations here
+        this.stompClient.send(
+          '/app/user.addUser',
+          {},
+          JSON.stringify({
+            nickName: nickname,
+            fullName: fullname,
+            status: 'ONLINE',
+          })
+        );
+      },
+      (error) => {
+        console.error('Error during connection:', error);
+        // Additional logic for handling connection error
+      }
+    );
   }
 
   sendMessage(message: string): void {
