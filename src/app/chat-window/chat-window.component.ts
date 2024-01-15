0ad17513d2ee6fbee848125f761836d0ea8e1de0
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2024-01-16 00:21:32 +0100

    UI for chat window

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-chat-window',
   templateUrl: './chat-window.component.html',
-  styleUrl: './chat-window.component.css'
+  styleUrl: './chat-window.component.css',
 })
 export class ChatWindowComponent {
+  message: any;
+  userFullName: any;
+  sendMessage() {
+    throw new Error('Method not implemented.');
+  }
 
+  //TODO
+  logout() {
+    throw new Error('Method not implemented.');
+  }
 }
